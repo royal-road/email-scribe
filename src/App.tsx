1879ea6f0b88c751aab2bf39cc004fec00ae3b98
finitevoid <fv@finitevoid.dev>
2024-08-20 02:02:30 -0400

    feat: Revamped scss to not jz be css and use scss primitives. Nested root into a 'newsletterDesigner' class.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import "./styles/App.scss";
 import { useTheme } from "./hooks/useTheme";
+import "./styles/styles.scss";
 
 function App() {
   const theme = useTheme();
   return (
-    <div data-theme={theme} className="bg-background text-foreground">
-      <div className="container">
-        <h1>Newsletter Builder</h1>
-        <p>Build your newsletter with ease!</p>
-      </div>
-    </div>
+    <div
+      data-theme={theme}
+      className="newsletterDesigner bg-background text-foreground"
+    ></div>
   );
 }
 
