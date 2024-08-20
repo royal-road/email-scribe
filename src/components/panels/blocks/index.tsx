c05dc8a955202ae9979c70679d689c78086024da
finitevoid <fv@finitevoid.dev>
2024-08-20 14:38:42 -0400

    feat: Added Collapsible and fully styled it (and cleaned up classes for it)

--- src/components/panels/blocks/index.tsx
+++ src/components/panels/blocks/index.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../../ui/Collapsible";
+import { ChevronsUpDown, X } from "lucide-react";
+import { Button } from "../../ui/Button";
+
 export default function BlocksPanel() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="BlocksPanel">
       <h2>Newsletter Designer</h2>
+
+      <div className="blocks">
+        <Collapsible open={open} onOpenChange={setOpen}>
+          <div className="collapsibleTrigger CollapsibleRepository">
+            <span>Block A</span>
+            <CollapsibleTrigger asChild>
+              <Button variant="outline">
+                {open ? <X /> : <ChevronsUpDown />}
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          {/* This is where the stuff I want to pre-display goes */}
+          <CollapsibleContent>
+            {/* This css class comes from Collapsible's own styles.scss*/}
+            <div className="CollapsibleRepository">
+              <span className="Text">Block A Option 1</span>
+            </div>
+            <div className="CollapsibleRepository">
+              <span className="Text">Block B Option 2</span>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
+      </div>
     </div>
   );
 }
---
+++ src/components/ui/Collapsible/index.tsx
@@ -0,0 +1,44 @@
+// src/components/ui/Collapsible/index.tsx
+
+import * as React from "react";
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+
+import { cn } from "../../../../lib/utils";
+
+const Collapsible = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.Root
+    ref={ref}
+    className={cn("CollapsibleRoot", className || "")}
+    {...props}
+  />
+));
+Collapsible.displayName = "Collapsible";
+
+const CollapsibleTrigger = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.Trigger
+    ref={ref}
+    className={cn("CollapsibleIconButton", className || "")}
+    {...props}
+  />
+));
+CollapsibleTrigger.displayName = CollapsiblePrimitive.Trigger.displayName;
+
+const CollapsibleContent = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.Content
+    ref={ref}
+    className={cn("CollapsibleContent", className || "")}
+    {...props}
+  />
+));
+CollapsibleContent.displayName = CollapsiblePrimitive.Content.displayName;
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
