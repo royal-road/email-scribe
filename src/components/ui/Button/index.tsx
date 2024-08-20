27bafefb65d462c08b71b3e4b913190098d2eaec
finitevoid <fv@finitevoid.dev>
2024-08-20 05:25:07 -0400

    feat: Created a shadcn-like button component

---
+++ src/components/ui/Button/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type ButtonVariant =
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "primary"
+  | "ghost"
+  | "link";
+type ButtonSize = "default" | "sm" | "lg" | "icon";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant = "default", size = "default", ...props }, ref) => {
+    const Comp = "button";
+    const buttonClasses = [
+      "button",
+      `button--${variant}`,
+      `button--${size}`,
+      className,
+    ]
+      .filter(Boolean)
+      .join(" ");
+
+    return <Comp className={buttonClasses} ref={ref} {...props} />;
+  }
+);
+
+Button.displayName = "Button";
+
+export { Button };
