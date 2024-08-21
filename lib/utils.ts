841f30119ab3d82896b0a83e2f8ed0a0396ee7bb
Suyash <suyashpratapts9@gmail.com>
2024-08-21 17:48:24 -0400

    chore: added a titleCase util and more feedback on button click

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,9 @@
 export function cn(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export function camelToTitleCase(str: string): string {
+  return str
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .replace(/\b\w/g, (char) => char.toUpperCase());
+}
