c99834c30147067e9af592f9e7dad8c58c31e0db
finitevoid <fv@finitevoid.dev>
2024-08-27 03:35:34 -0400

    chore: increased file size limit to accomodate somewhat bigger presets

--- api/server.ts
+++ api/server.ts
@@ -6,6 +6,7 @@ import {
   handlePresetSave,
   handlePresetList,
   handlePresetLoad,
+  handlePresetDelete,
 } from './routes/presets';
 
 const app = express();
@@ -13,7 +14,8 @@ const port = 8080;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+// app.use(express.urlencoded({ limit: '5mb' }));
 
 // Serve static files from the 'public' directory
 app.use(
@@ -29,6 +31,7 @@ app.post('/upload', handleUpload);
 app.post('/preset', handlePresetSave);
 app.get('/presets', handlePresetList);
 app.get('/preset', handlePresetLoad);
+app.delete('/preset', handlePresetDelete);
 
 // 404 handler
 app.use((req, res) => {
