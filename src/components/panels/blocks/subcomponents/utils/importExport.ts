a509f7b0b9c5ddd1fe6e55d02287507255d46362
finitevoid <fv@finitevoid.dev>
2024-08-27 03:39:22 -0400

    chore: Refactored import/output functionality out of UI

---
+++ src/components/panels/blocks/subcomponents/utils/importExport.ts
@@ -0,0 +1,34 @@
+import { BlockState } from '../..';
+import { Preset } from '../../hooks/presets';
+import { jsonToBlocks } from './blockInstancer';
+
+export const handleExport = (presetName: string, jsonString: string) => {
+  const preset = { presetName, data: jsonString } as Preset;
+  const blob = new Blob([JSON.stringify(preset)], {
+    type: 'application/json',
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = `${presetName}.json`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+};
+
+export const handleImport = (
+  event: React.ChangeEvent<HTMLInputElement>,
+  setBlocks: (blocks: BlockState[]) => void
+) => {
+  const file = event.target.files?.[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const content = e.target?.result as string;
+      const blocks = jsonToBlocks(content);
+      if (blocks) setBlocks(blocks);
+    };
+    reader.readAsText(file);
+  }
+};
