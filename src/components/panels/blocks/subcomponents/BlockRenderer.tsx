73ae35f2bd5cedee34ed9d662c45ba6a68a991cf
finitevoid <fv@finitevoid.dev>
2024-08-27 03:39:33 -0400

    chore: Prettier

--- src/components/panels/blocks/subcomponents/BlockRenderer.tsx
+++ src/components/panels/blocks/subcomponents/BlockRenderer.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-import { BlockInterface } from "../../../../blocks/setup/Types";
+import { useState } from 'react';
+import { BlockInterface } from '../../../../blocks/setup/Types';
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from "../../../ui/collapsible";
-import { Button } from "../../../ui/button";
-import { ChevronDown, ChevronUp } from "lucide-react";
-import Form from "@rjsf/core";
-import validator from "@rjsf/validator-ajv8";
-import ReorderDeleteGroup from "./ReorderDeleteGroup";
-import { camelToTitleCase } from "../../../../../lib/utils";
-import { RegistryWidgetsType } from "@rjsf/utils";
-import { FileUploadWidget } from "../../../ui/fileUploadWidget";
+} from '../../../ui/collapsible';
+import { Button } from '../../../ui/button';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import Form from '@rjsf/core';
+import validator from '@rjsf/validator-ajv8';
+import ReorderDeleteGroup from './ReorderDeleteGroup';
+import { camelToTitleCase } from '../../../../../lib/utils';
+import { RegistryWidgetsType } from '@rjsf/utils';
+import { FileUploadWidget } from '../../../ui/fileUploadWidget';
 
 interface BlockRendererProps {
   isTop: boolean;
@@ -42,8 +42,8 @@ export const BlockRenderer: React.FC<BlockRendererProps> = ({
   return (
     <Collapsible open={open} onOpenChange={setOpen}>
       <div
-        className="collapsibleTrigger CollapsibleRepository"
-        style={{ height: "3rem", paddingTop: "2rem", paddingBottom: "2rem" }}
+        className='collapsibleTrigger CollapsibleRepository'
+        style={{ height: '3rem', paddingTop: '2rem', paddingBottom: '2rem' }}
       >
         <ReorderDeleteGroup
           isBottom={isBottom}
@@ -52,9 +52,12 @@ export const BlockRenderer: React.FC<BlockRendererProps> = ({
           onUp={onUp}
           onDown={onDown}
         />
-        <span>{camelToTitleCase(block.meta.label)}</span>
+        <span style={{ fontSize: '1.5em' }}>
+          {camelToTitleCase(block.meta.label)}
+        </span>
+
         <CollapsibleTrigger asChild>
-          <Button variant="outline">
+          <Button variant='outline'>
             {open ? <ChevronUp /> : <ChevronDown />}
           </Button>
         </CollapsibleTrigger>
@@ -62,7 +65,7 @@ export const BlockRenderer: React.FC<BlockRendererProps> = ({
       {/* This is where the stuff I want to pre-display goes */}
       <CollapsibleContent>
         {/* This css class comes from Collapsible's own styles.scss*/}
-        <div className="CollapsibleRepository">
+        <div className='CollapsibleRepository'>
           <Form
             schema={block.schema}
             uiSchema={block.uiSchema}
@@ -70,7 +73,7 @@ export const BlockRenderer: React.FC<BlockRendererProps> = ({
             validator={validator}
             onChange={(e) => onChange(e.formData)}
             liveValidate={true}
-            className="blockForm"
+            className='blockForm'
             children={true}
             onError={(e) => console.error(e)}
             widgets={widgets}
