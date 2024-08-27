44194512939cd89d19414ed885b83b454f90550f
finitevoid <fv@finitevoid.dev>
2024-08-27 03:38:59 -0400

    chore: prettier

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,37 +1,38 @@
-import React from "react";
+import React from 'react';
 
 type ButtonVariant =
-  | "default"
-  | "destructive"
-  | "outline"
-  | "secondary"
-  | "primary"
-  | "ghost"
-  | "link";
-type ButtonSize = "default" | "sm" | "lg" | "icon";
+  | 'default'
+  | 'destructive'
+  | 'outline'
+  | 'secondary'
+  | 'primary'
+  | 'ghost'
+  | 'link';
+type ButtonSize = 'default' | 'sm' | 'lg' | 'icon';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
   size?: ButtonSize;
   asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant = "default", size = "default", ...props }, ref) => {
-    const Comp = "button";
+  ({ className, variant = 'default', size = 'default', ...props }, ref) => {
+    const Comp = 'button';
     const buttonClasses = [
-      "button",
+      'button',
       `button--${variant}`,
       `button--${size}`,
       className,
     ]
       .filter(Boolean)
-      .join(" ");
+      .join(' ');
 
     return <Comp className={buttonClasses} ref={ref} {...props} />;
   }
 );
 
-Button.displayName = "Button";
+Button.displayName = 'Button';
 
 export { Button };
--- src/components/ui/popover/index.tsx
+++ src/components/ui/popover/index.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import * as PopoverPrimitive from "@radix-ui/react-popover";
-import { X } from "lucide-react";
-import { cn } from "../../../../lib/utils";
-import { Button } from "../button";
+import * as React from 'react';
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+import { X } from 'lucide-react';
+import { cn } from '../../../../lib/utils';
+import { Button } from '../button';
 
 // - So, popover gets appended to body by default, but that causes css issues since it has to
 //  be encapsulated within .newsletterDesigner.
@@ -10,15 +10,17 @@ import { Button } from "../button";
 // not avaliable on the first render, making it so the popover is missing UI.
 // - Hence, I've added this state and useEffect to reInitialize popover with right container once
 // it's available.
-const spaClassName = ".newsletterDesigner";
+const spaClassName = '.newsletterDesigner';
 const Popover = PopoverPrimitive.Root;
 
 const PopoverTrigger = PopoverPrimitive.Trigger;
 
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, children, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & {
+    showClose?: boolean;
+  }
+>(({ className, showClose = true, children, ...props }, ref) => {
   const [container, setContainer] = React.useState<HTMLElement | null>(null);
 
   React.useEffect(() => {
@@ -36,23 +38,25 @@ const PopoverContent = React.forwardRef<
     <PopoverPrimitive.Portal container={container}>
       <PopoverPrimitive.Content
         ref={ref}
-        className={cn("PopoverContent", className || "")}
+        className={cn('PopoverContent', className || '')}
         {...props}
       >
         {children}
-        <PopoverPrimitive.Close
-          className="PopoverClose"
-          aria-label="Close"
-          asChild
-        >
-          <Button
-            variant="outline"
-            size="icon"
-            style={{ borderRadius: "100%", padding: "0.25rem" }}
+        {showClose && (
+          <PopoverPrimitive.Close
+            className='PopoverClose'
+            aria-label='Close'
+            asChild
           >
-            <X />
-          </Button>
-        </PopoverPrimitive.Close>
+            <Button
+              variant='outline'
+              size='icon'
+              style={{ borderRadius: '100%', padding: '0.25rem' }}
+            >
+              <X />
+            </Button>
+          </PopoverPrimitive.Close>
+        )}
       </PopoverPrimitive.Content>
     </PopoverPrimitive.Portal>
   );
