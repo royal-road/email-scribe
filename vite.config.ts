9a7f08a33ca909200150f2e08e7cbc6b802e8243
patnsr <patnsr.app@gmail.com>
2024-08-29 21:58:31 +0000

    fix: Built files now have a base url that might need to be better adapted for a general use case

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import sass from "sass";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/newsletter-builder",
   plugins: [react()],
   css: {
     preprocessorOptions: {
