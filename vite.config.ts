60aa9974758896f914365fd413b373c8d78f3610
finitevoid <fv@finitevoid.dev>
2024-09-07 04:45:30 -0400

    fix: Added vite.dts plugin for declarations and changes config accordingly

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import sass from 'sass';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -10,7 +11,7 @@ export default ({ mode }) => {
 
   return defineConfig({
     base: `/${process.env.VITE_BASE_PATH}`,
-    plugins: [react()],
+    plugins: [react(), dts()],
     css: {
       preprocessorOptions: {
         scss: {
