cc4812d297fb8c828b362c956542fe3bc02d025c
finitevoid <fv@finitevoid.dev>
2024-09-07 03:38:09 -0400

    fix: Removed unnecessary imports and turned react into peerDep

--- src/parser/utils/templater.ts
+++ src/parser/utils/templater.ts
@@ -1,5 +1,4 @@
 import Handlebars from 'handlebars';
-// import { inline } from "@css-inline/css-inline";
 
 // This first inlines all the CSS in the HTML, then compiles the Handlebars template with the formdata input by the user
 export function templatify(html: string, data: object, id?: string): string {
